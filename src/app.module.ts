fa0979b27bbff0a50923fac06b7966c4d817f5c4
Shirley Bailey <shirleybailey1002@outlook.com>
2026-04-03 00:50:31 +0300

    fix: register ActivationModule in AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { PromocodeModule } from './promocode/promocode.module';
+import { ActivationModule } from './activation/activation.module';
 
 @Module({
-  imports: [PrismaModule, PromocodeModule],
+  imports: [PrismaModule, PromocodeModule, ActivationModule],
   controllers: [AppController],
   providers: [AppService],
 })

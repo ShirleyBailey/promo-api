06d6787b3623d00cf785aa33c7611490ac12dc43
Shirley Bailey <shirleybailey1002@outlook.com>
2026-04-03 00:06:12 +0300

    feat: scaffold promocode module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
+import { PromocodeModule } from './promocode/promocode.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, PromocodeModule],
   controllers: [AppController],
   providers: [AppService],
 })
